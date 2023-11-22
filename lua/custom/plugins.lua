790c653e8e40a5df2d0c953b6957fc2e244545ce
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2023-11-22 22:54:20 +0100

    chore: remove noice

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -18,28 +18,6 @@ local plugins = {
   },
 
   {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {},
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "rcarriga/nvim-notify",
-    },
-    config = function()
-      require("noice").setup {
-        lsp = {
-          hover = {
-            enabled = false,
-          },
-          signature = {
-            enabled = false,
-          },
-        },
-      }
-    end,
-  },
-
-  {
     "williamboman/mason.nvim",
     opts = overrides.mason,
   },
