0ab8dbb1580df857d7f22bb554202edb427af52e
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2023-11-22 22:35:10 +0100

    chore: fix mappings

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -4,20 +4,20 @@ M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
     ["<D-s>"] = { ":w <CR>", "Save" },
-    ["<esc>"] = { ":noh <CR>", "Clear history", opts = {silent = true}}
+    ["<esc>"] = { ":noh <CR>", "Clear history", opts = { silent = true } },
   },
   v = {
-    [">"] = { ">gv", "indent" },
-    ["<D-s>"] = { ":w <CR>", "Save" },
+    [">"] = { ">gv", "indent", opts = { silent = true } },
+    ["<D-s>"] = { ":w <CR>", "Save", opts = { silent = true } },
   },
   i = {
-    ["<C-s>"] = { ":w <CR>", "Save" },
+    ["<C-s>"] = { ":w <CR>", "Save", opts = { silent = true } },
   },
 }
 
 M.lazygit = {
   n = {
-    ["<leader>gg"] = { ":LazyGit <CR>", "Open LazyGit" },
+    ["<leader>gg"] = { ":LazyGit <CR>", "Open LazyGit", opts = { silent = true } },
   },
 }
 
