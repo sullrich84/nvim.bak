3ccdf20a87345ff53e391e78a111a30b2abc5a50
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2023-11-22 21:59:50 +0100

    chore: add nvimtree mapping

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -1,26 +1,29 @@
----@type MappingsTable
 local M = {}
 
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
-    ["<D-s>"] = {":w <CR>", "Save"},
+    ["<D-s>"] = { ":w <CR>", "Save" },
   },
   v = {
-    [">"] = { ">gv", "indent"},
-    ["<D-s>"] = {":w <CR>", "Save"},
+    [">"] = { ">gv", "indent" },
+    ["<D-s>"] = { ":w <CR>", "Save" },
   },
   i = {
-    ["<C-s>"] = {":w <CR>", "Save"}
-  }
+    ["<C-s>"] = { ":w <CR>", "Save" },
+  },
 }
 
 M.lazygit = {
   n = {
-    ["<leader>gg"] = {"<cmd> LazyGit <CR>", "Open LazyGit"}
-  }
+    ["<leader>gg"] = { ":LazyGit <CR>", "Open LazyGit" },
+  },
 }
 
--- more keybinds!
+M.nvimtree = {
+  n = {
+    ["<leader>e"] = { ":NvimTreeToggle <CR>", "Open LazyGit" },
+  },
+}
 
 return M
