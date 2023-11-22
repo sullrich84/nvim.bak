37dd40d33e8395dd92e2d9aa30a7cde9d25be036
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2023-11-22 22:14:31 +0100

    chore: cleanup plugins

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,14 +1,9 @@
-local overrides = require("custom.configs.overrides")
+local overrides = require "custom.configs.overrides"
 
----@type NvPluginSpec[]
 local plugins = {
-
-  -- Override plugin definition options
-
   {
     "neovim/nvim-lspconfig",
     dependencies = {
-      -- format & linting
       {
         "jose-elias-alvarez/null-ls.nvim",
         config = function()
@@ -19,14 +14,12 @@ local plugins = {
     config = function()
       require "plugins.configs.lspconfig"
       require "custom.configs.lspconfig"
-    end, -- Override to setup mason-lspconfig
+    end,
   },
-
-  -- override plugin configs
 
   {
     "williamboman/mason.nvim",
-    opts = overrides.mason
+    opts = overrides.mason,
   },
 
   {
@@ -39,11 +32,15 @@ local plugins = {
     opts = overrides.nvimtree,
   },
 
-  -- Install custom plugins 
+   "NvChad/nvcommunity",
 
-  "NvChad/nvcommunity",
-  { import = "nvcommunity.git.lazygit" },
-  { import = "nvcommunity.editor.autosave" },
+  {
+    import = "nvcommunity.git.lazygit",
+  },
+
+  {
+    import = "nvcommunity.editor.autosave",
+  },
 
   {
     "max397574/better-escape.nvim",
@@ -95,24 +92,24 @@ local plugins = {
           enable = false,
         },
         shortcut = {
-          { icon = " ",  desc = "Projects", group = "Question",   action = "Telescope project",    key = "p", },
-          { icon = " ",  desc = "Recent",   group = "Question",   action = "Telescope oldfiles",   key = "r", },
-          { icon = " ",  desc = "Grep",     group = "Number",   action = "Telescope live_grep",  key = "g", },
-          { icon = "󰍉 ",  desc = "Find",     group = "Number",   action = "Telescope find_files", key = "f", },
-          { icon = " ",  desc = "Config",   group = "@comment", action = "e $MYVIMRC",           key = "c", },
-          { icon = " ",  desc = "Update",   group = "@comment", action = "Lazy update",          key = "u" },
-          { icon = "󰛯 ",  desc = "Health",   group = "@comment", action = "checkhealth",          key = "h", },
-          { icon = "󰩈 ",  desc = "Quit",     group = "@error",   action = "qa",                   key = "q", },
+          { icon = " ", desc = "Projects", group = "Question", action = "Telescope project", key = "p" },
+          { icon = " ", desc = "Recent", group = "Question", action = "Telescope oldfiles", key = "r" },
+          { icon = " ", desc = "Grep", group = "Number", action = "Telescope live_grep", key = "g" },
+          { icon = "󰍉 ", desc = "Find", group = "Number", action = "Telescope find_files", key = "f" },
+          { icon = " ", desc = "Config", group = "@comment", action = "e $MYVIMRC", key = "c" },
+          { icon = " ", desc = "Update", group = "@comment", action = "Lazy update", key = "u" },
+          { icon = "󰛯 ", desc = "Health", group = "@comment", action = "checkhealth", key = "h" },
+          { icon = "󰩈 ", desc = "Quit", group = "@error", action = "qa", key = "q" },
         },
         packages = {
-          enabled = false
+          enabled = false,
         },
         project = {
           enable = true,
           limit = 8,
           icon = " ",
           label = " Recent Projects:",
-          action = "Telescope find_files cwd="
+          action = "Telescope find_files cwd=",
         },
         mru = {
           enable = true,
@@ -129,10 +126,9 @@ local plugins = {
       },
     },
     dependencies = {
-      { "nvim-tree/nvim-web-devicons" }
-    }
+      { "nvim-tree/nvim-web-devicons" },
+    },
   },
-
 }
 
 return plugins
