813378dd3085cdd2ee9565dbaaaa881e5c04654c
Sebastian Ullrich [CBC-Extern] <sebastian.ullrich@rtl-extern.de>
2023-12-01 12:43:59 +0100

    chore: add working config to separate js lsps

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -1,12 +1,21 @@
 local null_ls = require "null-ls"
-
+local u = require "null-ls.utils"
 local b = null_ls.builtins
 
 local sources = {
-  -- webdev stuff
-  -- b.formatting.deno_fmt,
-  
-  b.formatting.prettier,
+  b.formatting.deno_fmt.with {
+    root_dir = u.root_pattern("deno.json", "deno.jsonc"),
+    condition = function(utils)
+      return utils.root_has_file { "deno.json", "deno.jsonc" }
+    end,
+  },
+
+  b.formatting.prettier.with {
+    root_dir = u.root_pattern "package.json",
+    condition = function(utils)
+      return utils.root_has_file { "package.json" }
+    end,
+  },
 
   -- Lua
   b.formatting.stylua,
