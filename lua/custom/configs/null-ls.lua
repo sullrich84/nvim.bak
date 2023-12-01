a9c2615bf24a14469ced546e0dfdc1073d34e6fb
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2023-12-01 16:17:00 +0100

    chore: renaming

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -1,10 +1,10 @@
 local null_ls = require "null-ls"
-local u = require "null-ls.utils"
+local utils = require "null-ls.utils"
 local b = null_ls.builtins
 
 local sources = {
   b.formatting.deno_fmt.with {
-    root_dir = u.root_pattern("deno.json", "deno.jsonc"),
+    root_dir = utils.root_pattern("deno.json", "deno.jsonc"),
     extra_args = { "--no-semicolons" },
     condition = function(utils)
       return utils.root_has_file { "deno.json", "deno.jsonc" }
@@ -12,7 +12,7 @@ local sources = {
   },
 
   b.formatting.prettier.with {
-    root_dir = u.root_pattern "package.json",
+    root_dir = utils.root_pattern "package.json",
     condition = function(utils)
       return utils.root_has_file { "package.json" }
     end,
