164b50523cae1c1f1e02b939371336e766591aa3
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2023-12-22 14:24:02 +0100

    chore: cleanup

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -235,14 +235,70 @@ local plugins = {
           enable = false,
         },
         shortcut = {
-          { icon = "  ", icon_hl="DashboardIconPrimary", desc = "Projects", group = "DashboardPrimary", action = "Telescope project", key = "p" },
-          { icon = "󰋚  ", icon_hl="DashboardIconPrimary", desc = "Recent", group = "DashboardPrimary", action = "Telescope oldfiles", key = "r" },
-          { icon = "  ", icon_hl="DashboardIconPrimary", desc = "Grep", group = "DashboardPrimary", action = "Telescope live_grep", key = "g" },
-          { icon = "󰍉  ", icon_hl="DashboardIconPrimary", desc = "Find", group = "DashboardPrimary", action = "Telescope find_files", key = "f" },
-          { icon = "  ", icon_hl="DashboardIconSecondary", desc = "Config", group = "DashboardSecondary", action = "e $MYVIMRC", key = "c" },
-          { icon = "󰚰  ", icon_hl="DashboardIconSecondary", desc = "Update", group = "DashboardSecondary", action = "Lazy update", key = "u" },
-          { icon = "󰛯  ", icon_hl="DashboardIconSecondary", desc = "Health", group = "DashboardSecondary", action = "checkhealth", key = "h" },
-          { icon = "󰩈  ", icon_hl="DashboardIconPrimary", desc = "Quit", group = "DashboardPrimary", action = "qa", key = "q" },
+          {
+            icon = "  ",
+            icon_hl = "DashboardIconPrimary",
+            desc = "Projects",
+            group = "DashboardPrimary",
+            action = "Telescope project",
+            key = "p",
+          },
+          {
+            icon = "󰋚  ",
+            icon_hl = "DashboardIconPrimary",
+            desc = "Recent",
+            group = "DashboardPrimary",
+            action = "Telescope oldfiles",
+            key = "r",
+          },
+          {
+            icon = "  ",
+            icon_hl = "DashboardIconPrimary",
+            desc = "Grep",
+            group = "DashboardPrimary",
+            action = "Telescope live_grep",
+            key = "g",
+          },
+          {
+            icon = "󰍉  ",
+            icon_hl = "DashboardIconPrimary",
+            desc = "Find",
+            group = "DashboardPrimary",
+            action = "Telescope find_files",
+            key = "f",
+          },
+          {
+            icon = "  ",
+            icon_hl = "DashboardIconSecondary",
+            desc = "Config",
+            group = "DashboardSecondary",
+            action = "e $MYVIMRC",
+            key = "c",
+          },
+          {
+            icon = "󰚰  ",
+            icon_hl = "DashboardIconSecondary",
+            desc = "Update",
+            group = "DashboardSecondary",
+            action = "Lazy update",
+            key = "u",
+          },
+          {
+            icon = "󰛯  ",
+            icon_hl = "DashboardIconSecondary",
+            desc = "Health",
+            group = "DashboardSecondary",
+            action = "checkhealth",
+            key = "h",
+          },
+          {
+            icon = "󰩈  ",
+            icon_hl = "DashboardIconPrimary",
+            desc = "Quit",
+            group = "DashboardPrimary",
+            action = "qa",
+            key = "q",
+          },
         },
         project = {
           enable = true,
@@ -257,10 +313,10 @@ local plugins = {
           icon = "󰋚 ",
           label = " Recent files:",
         },
-          footer = function()
-            local date = os.date("%A, %d. %B %Y %H:%M:%S")
-            return { "", date }
-          end,
+        footer = function()
+          local date = os.date "%A, %d. %B %Y %H:%M:%S"
+          return { "", date }
+        end,
       },
     },
     dependencies = {
